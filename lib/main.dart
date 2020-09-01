f90af9c6d9650fb7970d7a2f02f311d7a51f2b73
Basant Singh <you@example.com>
2020-09-01 20:12:43 +0530

    tab view with detailed screen

--- lib/main.dart
+++ lib/main.dart
@@ -32,20 +32,40 @@ class HomeScreen extends StatelessWidget {
           builder: (context) {
             return CupertinoPageScaffold(
               navigationBar: CupertinoNavigationBar(
-                middle: (i==0)?Text('Artiles'):Text('Views'),
+                middle: (i == 0) ? Text('Artiles') : Text('Views'),
               ),
               child: Center(
-                child:Text(
-                  "This is tab #$i",
-                  style:CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle
-                
-                ),
-                
-              ),
+                  child: CupertinoButton(
+                child: Text("This is tab #$i",
+                    style: CupertinoTheme.of(context)
+                        .textTheme
+                        .navLargeTitleTextStyle
+                        .copyWith(fontSize: 32)),
+                onPressed: () => Navigator.of(context)
+                    .push(CupertinoPageRoute(builder: (context) {
+                  return DetailedScreen(i == 0 ? 'Articles' : 'Views');
+                })),
+              )),
             );
           },
         );
       },
     );
+  }
+}
+
+class DetailedScreen extends StatelessWidget {
+  final String topic;
+  DetailedScreen(this.topic);
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(middle: Text("Details")),
+        child: Center(
+          child: Text(
+            "Details for $topic",
+            style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+          ),
+        ));
   }
 }
